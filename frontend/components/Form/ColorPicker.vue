935c2b256ddf4023f8930e2290d37a8cda0b4a56
eric <eric@shreve.dev>
2024-08-22 15:32:23 +0000

    feat: simple color picker
    - uses standard color input component

---
+++ frontend/components/Form/ColorPicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="form-control w-full">
+    <label class="label">
+      <span class="label-text">{{ label }}</span>
+    </label>
+    <input type="color" v-model="value" class="input input-primary w-full" />
+  </div>
+</template>
+
+  <script setup lang="ts">
+    const props = defineProps({
+      modelValue: {
+        type: String,
+        default: "#ff0000",
+      },
+      label: {
+        type: String,
+        default: "Select Color",
+      },
+    });
+  
+    const value = useVModel(props, "modelValue");
+  </script>
+
+  
